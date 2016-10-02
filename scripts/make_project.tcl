97e6b4edff52f8106869c14626bc21a28e568b53
apotocnik <anton.potocnik@gmail.com>
2016-10-02 13:08:42 +0200

    First commit of script files used to compile avel Demin's cores and projects

---
+++ scripts/make_project.tcl
@@ -0,0 +1,19 @@
+# ==================================================================================================
+# make_project.tcl
+#
+# Simple script for creating a vivado project from the project/ folder of the Pavel Demin's  
+# red-pitaya-notes-master/ git project
+#
+# Make sure the script is run from the red-pitaya-notes-master/ folder
+#
+# by Anton Potocnik, 02.10.2016
+# ==================================================================================================
+
+set project_name "rfid_ddc"
+
+set part_name "xc7z010clg400-1"
+
+set argv "$project_name $part_name"
+
+source scripts/project.tcl
+
