eee0e50b25b8616aeaca9cf8fc79ea778b57c01d
Anton Potocnik <anton.potocnik@gmail.com>
2016-11-21 08:57:06 +0100

    Added Project 3 - stopwatch

--- make_project.tcl
+++ make_project.tcl
@@ -1,17 +1,17 @@
 # ==================================================================================================
 # make_project.tcl
 #
-# Simple script for creating a vivado project from the project/ folder of the Pavel Demin's  
-# red-pitaya-notes-master/ git project
+# Simple script for creating a Vivado project from the project/ folder 
+# Based on Pavel Demin's red-pitaya-notes-master/ git project
 #
-# Make sure the script is run from the red-pitaya-notes-master/ folder
+# Make sure the script is executed from redpitaya_guide/ folder
 #
-# by Anton Potocnik, 02.10.2016 - 05.10.2016
+# by Anton Potocnik, 02.10.2016 - 21.11.2016
 # ==================================================================================================
 
 set project_name "1_led_blink"
 #set project_name "2_knight_rider"
-#set project_name "3_timing_knight_rider"
+#set project_name "3_stopwatch"
 
 source projects/$project_name/block_design.tcl
 
