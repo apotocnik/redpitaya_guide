259a11b727cdeda2f412a696522642ff51db47ac
apotocnik <anton.potocnik@gmail.com>
2016-11-30 00:37:23 +0100

    Remove comments in basic_red_pitaya_bd.tcl

--- projects/4_averager/basic_red_pitaya_bd.tcl
+++ projects/4_averager/basic_red_pitaya_bd.tcl
@@ -1,11 +1,11 @@
 
 # ==================================================================================================
-# block_design.tcl - Create Vivado Project - 3_stopwatch
+# block_design.tcl - Create Vivado Project - basic red pitaya block design
 #
 # This script should be run from the base redpitaya-guides/ folder inside Vivado tcl console.
 #
 # This script is modification of Pavel Demin's project.tcl and block_design.tcl files
-# by Anton Potocnik, 20.11.2016
+# by Anton Potocnik, 29.11.2016
 # Tested with Vivado 2016.3
 # ==================================================================================================
 
@@ -58,35 +58,6 @@ set_property -dict [list CONFIG.C_BUF_TYPE {OBUFDS}] [get_bd_cells util_ds_buf_2
 endgroup
 
 
-# Define all 8 LEDs
-#set_property LEFT 7 [get_bd_ports led_o]
-
-# slice LEDs
-#startgroup
-#create_bd_cell -type ip -vlnv xilinx.com:ip:xlslice:1.0 xls_LEDs
-#set_property -dict [list CONFIG.DIN_TO {24} CONFIG.DIN_FROM {31} CONFIG.DIN_WITDH {32} CONFIG.DOUT_WIDTH {8}] [get_bd_cells xls_LEDs]
-#endgroup
-
-
-# slice CE
-#startgroup
-#create_bd_cell -type ip -vlnv xilinx.com:ip:xlslice:1.0 xls_CE
-#set_property -dict [list CONFIG.DIN_TO {0} CONFIG.DIN_FROM {0} CONFIG.DIN_WITDH {32} CONFIG.DOUT_WIDTH {1}] [get_bd_cells xls_CE]
-#endgroup
-
-# slice SCLR
-#startgroup
-#create_bd_cell -type ip -vlnv xilinx.com:ip:xlslice:1.0 xls_SCLR
-#set_property -dict [list CONFIG.DIN_TO {1} CONFIG.DIN_FROM {1} CONFIG.DIN_WITDH {32} CONFIG.DOUT_WIDTH {1}] [get_bd_cells xls_SCLR]
-#endgroup
-
-# Binary Counter - 32bit
-#startgroup
-#create_bd_cell -type ip -vlnv xilinx.com:ip:c_counter_binary:12.0 c_counter_binary_0
-#set_property -dict [list CONFIG.Output_Width {32} CONFIG.CE {true} CONFIG.SCLR {true}] [get_bd_cells c_counter_binary_0]
-#endgroup
-
-
 # AXI GPIO IP core
 startgroup
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 axi_gpio_0
@@ -118,20 +89,9 @@ set_property -dict [list CONFIG.PartsPresent {Phase_Generator_and_SIN_COS_LUT} C
 endgroup
 
 
-# ====================================================================================
-# RTL modules
-
-# Add rtl module: signal route
-#create_bd_cell -type module -reference signal_route signal_route_0
-
-
-# Add rtl modules - averager
-#create_bd_cell -type module -reference averager averager_0
-
 
 # ====================================================================================
 # Connections 
-
 
 connect_bd_net [get_bd_ports daisy_p_i] [get_bd_pins util_ds_buf_1/IBUF_DS_P]
 connect_bd_net [get_bd_ports daisy_n_i] [get_bd_pins util_ds_buf_1/IBUF_DS_N]
@@ -169,15 +129,6 @@ connect_bd_intf_net [get_bd_intf_pins dds_compiler_0/M_AXIS_DATA] [get_bd_intf_p
 connect_bd_net [get_bd_pins clk_wiz_0/clk_in1] [get_bd_pins dds_compiler_0/aclk]
 connect_bd_net [get_bd_pins dds_compiler_0/aclk] [get_bd_pins axis_red_pitaya_adc_0/adc_clk]
 
-
-
-
-#connect_bd_net [get_bd_pins xls_SCLR/Din] [get_bd_pins axi_gpio_0/gpio_io_o]
-#connect_bd_net [get_bd_pins xls_CE/Din] [get_bd_pins axi_gpio_0/gpio_io_o]
-#connect_bd_net [get_bd_pins axi_gpio_0/gpio_io_i] [get_bd_pins axi_gpio_0/gpio_io_o]
-#connect_bd_net [get_bd_pins xls_CE/Dout] [get_bd_pins c_counter_binary_0/CE]
-#connect_bd_net [get_bd_pins xls_SCLR/Dout] [get_bd_pins c_counter_binary_0/SCLR]
-#connect_bd_net [get_bd_pins axi_gpio_0/gpio2_io_i] [get_bd_pins c_counter_binary_0/Q]
 
 
 apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP0" Clk "Auto" }  [get_bd_intf_pins axi_gpio_0/S_AXI]
